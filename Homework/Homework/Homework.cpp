d766a61fa5a3fa83f7084808051f23233685fb7a
night-coder44 <eneskolak44@gmail.com>
2025-10-24 23:39:19 +0300

    a

---
+++ Homework/Homework/Homework.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Ilk sayiyi giriniz : ";
+    int firstNumber;
+    cin >> firstNumber;
+
+    cout << "Ikinci sayiyi giriniz : ";
+    int secondNumber;
+    cin >> secondNumber;
+
+    cout << "Iki sayinin toplami : " << firstNumber + secondNumber << endl;
+    cout << "Iki sayinin farki : " << firstNumber - secondNumber << endl;
+    cout << "Iki sayinin carpimi : " << firstNumber * secondNumber << endl;
+    cout << "Iki sayinin bolumu : " << firstNumber / secondNumber << endl;
+
+
+
+}
